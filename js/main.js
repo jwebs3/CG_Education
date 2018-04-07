06a282918e98b7f52630dac3891860c9c4314357
Jessica <jrw290s@gmail.com>
2018-04-06 21:50:12 -0500

    styled teachers page, added js file

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log("This file is linked!");
+
+var Teacher = "Sally Jones";
+var Department = "Physics";
+var Rating1 = 3.4;
+var Rating2 = 5.0;
+var Rating3 = 4.2;
+
+console.log(Teacher);
+console.log(Department);
+console.log(Rating1, Rating2, Rating3);
