c5367a51da01ac9593ca4cda7c05a4ebb4d480e9
Jessica <jrw290s@gmail.com>
2018-04-11 19:11:34 -0500

    finished js challenge 1

--- js/main.js
+++ js/main.js
@@ -5,7 +5,30 @@ var Department = "Physics";
 var Rating1 = 3.4;
 var Rating2 = 5.0;
 var Rating3 = 4.2;
+var avgRating = (Rating1+Rating2+Rating3);
 
 console.log(Teacher);
 console.log(Department);
 console.log(Rating1, Rating2, Rating3);
+console.log(Math.round(10*avgRating/3)/10);
+
+var Student = "Sally Jones";
+var Major = "Major";
+var Email = "sally@cg.edu";
+var Gpa = 6.0;
+var avgGpa = Gpa;
+var StudentCourse1 = "Calculus";
+var StudentCourse2 = "Econ";
+
+console.log(Student);
+console.log(Major);
+console.log(Math.round(10*avgGpa)/10);
+console.log(StudentCourse1, StudentCourse2);
+
+var TeacherCourse1 = "Astronomy";
+var TeacherCourse2 = "Physics";
+var Semester = "Fall 2017";
+
+console.log(TeacherCourse1, TeacherCourse2);
+console.log(Teacher);
+console.log(Semester);
