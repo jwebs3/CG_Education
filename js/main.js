f052410f0c3abff366ec29595fe50e81dcf50d5a
Jessica <jrw290s@gmail.com>
2018-06-13 12:53:56 -0500

    finish JS3

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var department = "Physics";
 var rating1 = 3.4;
 var rating2 = 5.0;
 var rating3 = 4.2;
-var avgRating = (Rating1+Rating2+Rating3);
+var avgRating = (rating1+rating2+rating3);
 
 console.log(teacher);
 console.log(department);
@@ -49,28 +49,67 @@ function addTeacherRating (teacherRating) {
   teacherRating.push(4.5);
   return teacherRating;
 }
+console.log(teacherRating);
 
-var review = window.prompt("We would like for you to review. Please enter a rating between 0.0-5.0?");
+//var review = window.prompt("We would like for you to review. Please enter a rating between 0.0-5.0?");
 
-if (review >= 0.0, review <= 5.0) {
-  teacherRating.push(review);
-  alert("'Thanks for your review!' var Teacher 'average rating is now' var avg '.'");
-} else {
-  window.prompt("We would like for you to review. Please enter a rating between 0.0-5.0?");
+//if (review >= 0.0, review <= 5.0) {
+//  teacherRating.push(review);
+// alert("Thanks for your review!" + teacher + "average rating is now" + avg ".");
+//} else {
+//  window.prompt("We would like for you to review. Please enter a rating between 0.0-5.0?");
+//}
+
+var courses = [["Astronomy", "Physics"], ["Calculus", "Physics"], ["Statistics", "Physics"], ["Writing", "English"]];
+var department = "Physics";
+
+var departmentFilter = function filterByDepartment(courses, department) {
+  if (department == courses [0][0]) {
+    return courses;
+  } else if (department == courses [0][1]) {
+    return courses;
+  } else if (department == courses [0][2]) {
+    return courses;
+  } else if (department == courses [0][3]) {
+    return courses;
+  } else {
+    return undefined;
+  }
+var newCourses = courses.filter(departmentFilter);
+console.log(newCourses);
+
+//function departmentFilter()
+
+//  var coursesType = courses.filter(
+//    (callback(courses[0][1] == department, courses[1][1] == department, courses[2][1] == department, courses[3][1] == department);
+//  return coursesType;
+//}
+//console.log(departmentFilter(courses));
+
+var allDepartments = ["Physics", "History", "English", "Math", "Science"];
+var physicsClasses = ["Astronomy", "Calculus", "Statistics"];
+var historyClasses = ["WWII", "WWI", "Sparta"];
+var englishClasses = ["Writing", "Linguistics", "Teaching"];
+var mathClasses = ["Algebra", "Geometry", "Calculus"];
+var scienceClasses = ["Biology", "Chemistry", "Organic Chemistry"];
+
+var departmentChoice = window.prompt("What department are you looking for a course in?");
+
+function classReturn(departmentChoice) {
+if (departmentChoice == allDepartments[0]) {
+  alert(physicsClasses);
+} else if (departmentChoice == allDepartments[1]) {
+  alert(historyClasses);
+} else if (departmentChoice == allDepartments[2]) {
+  alert(englishClasses);
+} else if (departmentChoice == allDepartments[3]) {
+  alert(mathClasses)
+} else if (departmentChoice == allDepartments[4]) {
+  alert(scienceClasses);
+} else
+  window.prompt("What department are you looking for a course in?");
 }
 
-var courses [
-  [Astronomy, Physics],
-  [Calculus, Physics],
-  [Statistics, Physics],
-  [WWII, History]
-];
-var department = ;
 
-function (courses, department) {
-  if (courses[0][1] = Physics, courses[1][1] = Physics, courses[2][1] = Physics, courses[3][1] = Physics);
-  return(var coursesDepartment);
-  console.log(coursesDepartment);
-} else {
-  return undefined;}
-}
+
+//console.log(classReturn(departmentChoice));
